2ac18e9b6b2e65594fa84bb95eace0da2204677f
RonexLemon <ronexondimu@gmail.com>
2024-05-02 08:35:37 +0300

    chore:shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen  flex-col items-center p-24">
-     <div>Reusable Components</div>
+     
      <div className="flex w-full justify-around items-center">
      <Button onClick={()=>alert("yollow")} size='sm' variant='secondary'>Button</Button>
       <Button onClick={()=>alert("yollow")} size='md'>Click Me</Button>
